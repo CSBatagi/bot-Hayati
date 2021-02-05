d1bcd4a0eacfd5c3d2fd3413f79926260ce3354e
baytuni <oytun.peksel@meltwater.com>
2021-02-05 12:30:49 +0100

    keyword degisiklikleri

--- init.py
+++ init.py
@@ -30,17 +30,22 @@ async def on_message(message):
     #await message.channel.send('Selam moruklar ben Hayati')
         msg = message.content.lower() 
         if "gelen" in msg and ("say" in msg or "liste" in msg):    
+
             liste = sheet.get(SPREADSHEET_ID, PLAYER_RANGE ) 
             join_list = sheet.get(SPREADSHEET_ID, JOIN_RANGE) 
             msg_bck ="\n" #+ "\n".join(["".join(a) for i, a  in enumerate(liste) if join_list[i]])
             toplam = 0
+
             for i, isim in enumerate(liste):
                 if join_list[i] == ['TRUE']:
                     msg_bck += "".join(isim) + "\n"
                     toplam += 1
+
             msg_bck = f"Al A.Q toplam {toplam} kisi geliyor:\n" + msg_bck
             await message.channel.send(msg_bck)
-        elif "ekle" in msg or "geliyorum" in msg or "gelicem" in msg:
+
+        elif "ekle" in msg or "geliyo" in msg or "gelice" in msg:
+
             liste = sheet.get(SPREADSHEET_ID, PLAYER_RANGE ) 
             join_list = sheet.get(SPREADSHEET_ID, JOIN_RANGE) 
             not_join_list = sheet.get(SPREADSHEET_ID, NOT_JOIN_RANGE)
@@ -53,12 +58,14 @@ async def on_message(message):
                     sheet.update(SPREADSHEET_ID, NOT_JOIN_RANGE, not_join_list)
                     await message.channel.send("Bu bebeyi ekledim: " + "".join(isim))
                     return 
+
             await message.channel.send("Böyle biri listede yok ki a.q napam ben simdi?") 
 
-        elif "cikar" in msg or "çıkar" in msg or "gelmiyor" in msg or "gelmice" in msg:
+        elif "cikar" in msg or "çıkar" in msg or "gelmiyo" in msg or "gelmice" in msg:
             liste = sheet.get(SPREADSHEET_ID, PLAYER_RANGE ) 
             join_list = sheet.get(SPREADSHEET_ID, JOIN_RANGE) 
             not_join_list = sheet.get(SPREADSHEET_ID, NOT_JOIN_RANGE) 
+
             for i, isim in enumerate(liste):
                 if "".join(isim).lower() in msg:
                     join_list[i] = ['FALSE']
@@ -67,43 +74,13 @@ async def on_message(message):
                     sheet.update(SPREADSHEET_ID, NOT_JOIN_RANGE, not_join_list)
                     await message.channel.send("Bu iti listeden cikardim, siktirsin gitsin aq cocugu: " + "".join(isim))
                     return 
-            await message.channel.send("".join(isim) + " listede yok ki a.q napam ben simdi?") 
+
+            await message.channel.send("Böyle biri listede yok ki a.q napam ben simdi?") 
 
         else:
             await message.channel.send("Buyur abi?")
+
     except:
         raise discord.DiscordException
-
             
-    # # Restrict the command to a role
-    # # Change REQUIREDROLE to a role id or None
-    # REQUIREDROLE = None
-    # if REQUIREDROLE is not None and discord.utils.get(message.author.roles, id=str(REQUIREDROLE)) is None:
-    #     await message.channel.send('You don\'t have the required role!')
-    #     return
-
-    # # Command to insert data to excel
-    # if message.content.startswith('!s '):
-
-    #     msg = message.content[3:]
-    #     result = [x.strip() for x in msg.split(',')]
-    #     if len(result) == FIELDS:
-    #         # Add
-    #         print(message.created_at)
-    #         DATA = [message.author.name] + [str(message.author.id)] + [str(message.created_at)] + result
-    #         sheet.add(SPREADSHEET_ID, RANGE_NAME, DATA)
-    #         await message.channel.send('Your data has been successfully submitted!')
-    #     else:
-    #         # Needs more/less fields
-    #         await message.channel.send('Error: You need to add {0} fields, meaning it can only have {1} comma.'.format(FIELDS,FIELDS-1))
-    
-    # # Whois
-    # # Please dont remove the copyright and github repo
-    # elif len(message.mentions) > 0:
-    #     for muser in message.mentions:
-    #         if muser.id == client.user.id:
-    #             if any(word in message.content for word in ['whois','who is','Help','help','info']):
-    #                 await message.channel.send('This bot was made by hugonun(https://github.com/hugonun/).\nSource code: https://github.com/hugonun/discord2sheet-bot')
-
-
 client.run(BOT_TOKEN) # Add bot token here
